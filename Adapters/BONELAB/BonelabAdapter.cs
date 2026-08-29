039c294f58205043cec6dfd1365782eccb725ebd
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-29 17:28:10 -0400

    Add BonelabAdapter class for BONELAB integration

---
+++ Adapters/BONELAB/BonelabAdapter.cs
@@ -0,0 +1,8 @@
+namespace UniversalVR.Adapters.BONELAB;
+
+public sealed class BonelabAdapter : StubAdapterBase
+{
+    public override string Id => "bonelab";
+
+    public override string DisplayName => "BONELAB";
+}
