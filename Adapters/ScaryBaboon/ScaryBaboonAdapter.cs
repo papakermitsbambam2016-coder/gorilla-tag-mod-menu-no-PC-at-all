89814a285b6f5d7149cbae653d03344a36a164bd
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-29 17:30:02 -0400

    Add ScaryBaboonAdapter class implementation

---
+++ Adapters/ScaryBaboon/ScaryBaboonAdapter.cs
@@ -0,0 +1,8 @@
+namespace UniversalVR.Adapters.ScaryBaboon;
+
+public sealed class ScaryBaboonAdapter : StubAdapterBase
+{
+    public override string Id => "scary-baboon";
+
+    public override string DisplayName => "Scary Baboon";
+}
