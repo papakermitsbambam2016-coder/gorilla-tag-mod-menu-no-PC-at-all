476d8454034ab0695639eed59e34f64c277cf197
papakermitsbambam2016-coder <papakermitsbambam2016@gmail.com>
2026-08-29 17:28:57 -0400

    Add GorillaTagAdapter class for Gorilla Tag support

---
+++ Adapters/GorillaTag/GorillaTagAdapter.cs
@@ -0,0 +1,8 @@
+namespace UniversalVR.Adapters.GorillaTag;
+
+public sealed class GorillaTagAdapter : StubAdapterBase
+{
+    public override string Id => "gorilla-tag";
+
+    public override string DisplayName => "Gorilla Tag";
+}
